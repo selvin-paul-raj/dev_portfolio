b22ce031fbe311b9dba9fda0277fa898c8828a81
Selvin PaulRaj <115263986+selvin-paul-raj@users.noreply.github.com>
2024-10-25 21:07:15 +0530

    add:Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,15 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-[#FAFAF9] text-gray-950 relative pt-20 dark:bg-[#000] dark:text-white dark:text-opacity-80`}
       >
+        <script
+        dangerouslySetInnerHTML={{
+          __html: `(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "oo7sqjfgyt");`
+        }}
+      />
         <div className="bg-[#805c8b] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#000]"></div>
         <div className="bg-[#CDECFF] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#000]"></div>
 
